2ab17085c81a633d6fa97fe0a83a84cf22f11cd1
ZhangYuan <zhang.yuan@synyi.com>
2026-04-17 16:34:26 +0800

    11

---
+++ src/Unicorn.ViewManager/AutoHideChannelControl.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Unicorn.ViewManager
+{
+    public class AutoHideChannelControl : CustomItemsControl
+    {
+        public AutoHideRootControl ParentHost
+        {
+            get;
+            internal set;
+        }
+
+        public object AutoHideSlideout
+        {
+            get
+            {
+                return (object)GetValue(AutoHideSlideoutProperty);
+            }
+            set
+            {
+                SetValue(AutoHideSlideoutProperty, value);
+            }
+        }
+        public static readonly DependencyProperty AutoHideSlideoutProperty = DependencyProperty.Register("AutoHideSlideout", typeof(object), typeof(AutoHideChannelControl), new PropertyMetadata(null));
+
+
+        public static Dock GetChannelDock(DependencyObject obj)
+        {
+            return (Dock)obj.GetValue(ChannelDockProperty);
+        }
+
+        public static void SetChannelDock(DependencyObject obj, Dock value)
+        {
+            obj.SetValue(ChannelDockProperty, value);
+        }
+
+        public static readonly DependencyProperty ChannelDockProperty = DependencyProperty.RegisterAttached("ChannelDock", typeof(Dock), typeof(AutoHideChannelControl), new PropertyMetadata(System.Windows.Controls.Dock.Left));
+
+        static AutoHideChannelControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AutoHideChannelControl), new FrameworkPropertyMetadata(typeof(AutoHideChannelControl)));
+        }
+
+
+        public AutoHideChannelControl()
+        {
+
+        }
+
+        protected override void OnItemsChanged(NotifyCollectionChangedEventArgs e)
+        {
+            base.OnItemsChanged(e);
+
+            if (e.OldItems != null)
+            {
+                foreach (var item in e.OldItems)
+                {
+                    if (item is AutoHideChannelItem channelItem)
+                    {
+                        channelItem.ParentHost = null;
+                    }
+                }
+            }
+
+            foreach (var item in this.Items)
+            {
+                if (item is AutoHideChannelItem channelItem)
+                {
+                    AutoHideChannelControl.SetChannelDock(channelItem, AutoHideChannelControl.GetChannelDock(this));
+                    channelItem.ParentHost = this;
+                }
+                else
+                {
+                    throw new NotSupportedException();
+                }
+            }
+        }
+
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is AutoHideChannelItem;
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new AutoHideChannelItem();
+        }
+
+        public void Dock(AutoHideChannelItem channelitem)
+        {
+            if (!this.Items.Contains(channelitem))
+            {
+                this.Items.Add(channelitem);
+            }
+        }
+
+        public void UnDock(DependencyObject item)
+        {
+            this.Items.Remove(item);
+        }
+    }
+}
---
+++ src/Unicorn.ViewManager/AutoHideChannelItem.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using Unicorn.ViewManager.Internal;
+
+namespace Unicorn.ViewManager
+{
+    public class AutoHideChannelItem : Button
+    {
+        public object Title
+        {
+            get
+            {
+                return (object)GetValue(TitleProperty);
+            }
+            set
+            {
+                SetValue(TitleProperty, value);
+            }
+        }
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(object), typeof(AutoHideChannelItem), new PropertyMetadata(null));
+
+        public AutoHideChannelControl ParentHost
+        {
+            get;
+            internal set;
+        }
+
+        private readonly ToolTabGroupControl _toolTabGroupControl = new ToolTabGroupControl();
+        private readonly AutoHideWindow _autoHideWindow = new AutoHideWindow()
+        {
+            MinHeight = 20,
+            MinWidth = 20
+        };
+        static AutoHideChannelItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AutoHideChannelItem), new FrameworkPropertyMetadata(typeof(AutoHideChannelItem)));
+        }
+
+        public AutoHideChannelItem()
+        {
+            this._toolTabGroupControl.ItemsChanged += _toolTabGroupControl_ItemsChanged;
+            this._autoHideWindow.Content = this._toolTabGroupControl;
+
+            AutoHideManager.RegisterChannelItem(this);
+            this.Click += AutoHideChannelItem_Click;
+        }
+
+        private void _toolTabGroupControl_ItemsChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (this._toolTabGroupControl.Items.Count == 0)
+            {
+                this.UnDock();
+            }
+        }
+
+        private void AutoHideChannelItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.ParentHost.AutoHideSlideout == this._autoHideWindow)
+            {
+                this.ParentHost.AutoHideSlideout = null;
+            }
+            else
+            {
+                var parentdock = AutoHideChannelControl.GetChannelDock(this.ParentHost);
+
+                switch (parentdock)
+                {
+                    case System.Windows.Controls.Dock.Left:
+                    case System.Windows.Controls.Dock.Right:
+                        if (this._autoHideWindow.Width.IsNonreal())
+                        {
+                            this._autoHideWindow.Width = 300;
+                        }
+                        break;
+
+                    case System.Windows.Controls.Dock.Top:
+                    case System.Windows.Controls.Dock.Bottom:
+                        if (this._autoHideWindow.Width.IsNonreal())
+                        {
+                            this._autoHideWindow.Height = 300;
+                        }
+                        break;
+                }
+
+                AutoHideChannelControl.SetChannelDock(this._autoHideWindow, parentdock);
+
+                this.ParentHost.AutoHideSlideout = this._autoHideWindow;
+            }
+        }
+
+        public void Dock(TabGroupTabItem tabItem)
+        {
+            tabItem.UnDock();
+            this._toolTabGroupControl.Dock(tabItem);
+        }
+
+        public void UnDock()
+        {
+            if (this.ParentHost != null)
+            {
+                if (this.ParentHost.AutoHideSlideout == this._autoHideWindow)
+                {
+                    this.ParentHost.AutoHideSlideout = null;
+                }
+                this.ParentHost.UnDock(this);
+            }
+        }
+    }
+}
